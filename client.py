5d1bcf7412eff5c77fa2cfa52fc2e5a800a5da73
Ther <thor.demeestere@telenet.be>
2023-10-08 21:27:23 +0200

    f

--- client.py
+++ client.py
@@ -2,14 +2,22 @@ from client_library import connect_to_server
 
 
 client_socket = connect_to_server("192.168.32.156", 12345)
+previous_state = False
+
 
 while True:
-    message = input("Enter a message to send to the server (or type 'exit' to quit): ")
+    current_state = keyboard.is_pressed("x")  # Get the current state of the "x" key
     
-    if message == 'exit':
-        break
+    if current_state != previous_state:  # Compare current state to previous state
+        if current_state:
+            message = "press:t"
+            client_socket.send(message.encode('utf-8'))
+        else:
+            message = "release:t"
+            client_socket.send(message.encode('utf-8'))
+    
+    previous_state = current_state  # Update the previous state
 
-    client_socket.send(message.encode('utf-8'))
+
 
 client_socket.close()
-
