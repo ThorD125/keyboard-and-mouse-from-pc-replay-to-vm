21dd66682f362e4601e49c855b6f1ed400c86141
Ther <thor.demeestere@telenet.be>
2023-10-08 22:02:28 +0200

    f

--- client.py
+++ client.py
@@ -3,8 +3,8 @@ import keyboard
 
 # client_socket = connect_to_server("192.168.32.156", 12345)
 client_socket = connect_to_server("127.0.0.1", 12345)
-previous_state = False
 
+previous_state = False
 
 while True:
     current_state = keyboard.is_pressed("x")
--- server.py
+++ server.py
@@ -4,16 +4,22 @@ from server_library import create_server_socket
 server_socket = create_server_socket('127.0.0.1', 12345)
 
 client_socket, client_address = server_socket.accept()
+
+
+state = False
+
 while True:
     data = client_socket.recv(1024).decode('utf-8')
     
     if not data:
-        break  
-    
-    print(f"Received: {data}")
-    
+        break
 
     
+    
+    print(f"Received: {data}")
+
+
+
 
 client_socket.close()
 server_socket.close()
