e1878a90adae10f89ac70e4869d53221a92dd553
Ther <thor.demeestere@telenet.be>
2023-10-08 22:48:02 +0200

    f

--- server.py
+++ server.py
@@ -4,13 +4,8 @@ import pyautogui
 import time
 import keyboard
 
-# server_socket = create_server_socket('192.168.32.156', 12345)
-server_socket = create_server_socket('127.0.0.1', 12345)
-
-client_socket, client_address = server_socket.accept()
-
-
-
+server_socket = create_server_socket('192.168.32.156', 12345)
+# server_socket = create_server_socket('127.0.0.1', 12345)
 
 window = pygetwindow.getWindowsWithTitle("Warframe")[0]
 window.activate()
@@ -19,6 +14,13 @@ center_x = screen_width // 2
 center_y = screen_height // 2
 time.sleep(1)
 pyautogui.click(center_x, center_y)
+
+
+client_socket, client_address = server_socket.accept()
+
+
+
+
 
 
 
@@ -35,6 +37,6 @@ while True:
     
     print(f"Received: {data}")
 
-
 client_socket.close()
 server_socket.close()
+
