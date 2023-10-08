63166210e1e1197fad934d091cf1b931d485892c
Ther <thor.demeestere@telenet.be>
2023-10-09 00:56:38 +0200

    f

--- client.py
+++ client.py
@@ -4,17 +4,17 @@ import keyboard
 client_socket = connect_to_server("192.168.32.156", 12345)
 # client_socket = connect_to_server("127.0.0.1", 12345)
 
-previous_state = False
+previous_state_w = False
 
 while True:
-    current_state = keyboard.is_pressed("w")
-    if current_state != previous_state:
-        if current_state:
+    current_state_w = keyboard.is_pressed("w")
+    if current_state_w != previous_state_w:
+        if current_state_w:
             message = "press:w"
             client_socket.send(message.encode('utf-8'))
         else:
             message = "release:w"
             client_socket.send(message.encode('utf-8'))
-    previous_state = current_state
+    previous_state_w = current_state_w
 
 client_socket.close()
--- server.py
+++ server.py
@@ -12,16 +12,10 @@ window.activate()
 screen_width, screen_height = pyautogui.size()
 center_x = screen_width // 2
 center_y = screen_height // 2
-time.sleep(1)
-pyautogui.click(center_x, center_y)
 
 
 client_socket, client_address = server_socket.accept()
-
-
-
-
-
+pyautogui.click(center_x, center_y)
 
 
 while True:
