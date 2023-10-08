5976c852ee62fd8efdd3d15963ea137db50dbc63
Ther <thor.demeestere@telenet.be>
2023-10-09 01:03:05 +0200

    f

--- server.py
+++ server.py
@@ -4,8 +4,10 @@ import pyautogui
 import time
 import keyboard
 
+
 server_socket = create_server_socket('192.168.32.156', 12345)
 # server_socket = create_server_socket('127.0.0.1', 12345)
+
 
 window = pygetwindow.getWindowsWithTitle("Notepad")[0]
 window.activate()
@@ -15,21 +17,9 @@ center_y = screen_height // 2
 
 
 client_socket, client_address = server_socket.accept()
-pyautogui.click(center_x, center_y)
 
 
-# while True:
-#     data = client_socket.recv(1024).decode('utf-8')
-    
-#     if not data: 
-#         break
-
-#     if data == "press:w":
-#         press("w")
-#     elif data == "release:w":
-#         release("w")
-    
-#     print(f"Received: {data}")
+pyautogui.click(center_x, center_y)    
 
 while True:
     data = client_socket.recv(1024).decode('utf-8')
@@ -41,6 +31,8 @@ while True:
         keyboard.press(data.split(":")[1])
     elif "release" in data:
         keyboard.release(data.split(":")[1])
+    print(f"Received: {data}")
+
 
 client_socket.close()
 server_socket.close()
