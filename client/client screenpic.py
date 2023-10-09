eeba8478ed74ce80f718b453bd801d5cf580ebd6
Ther <thor.demeestere@telenet.be>
2023-10-09 11:26:52 +0200

    f

---
+++ client/client screenpic.py
@@ -0,0 +1,38 @@
+import socket
+import pyautogui
+from PIL import ImageGrab
+
+# Define server address and port
+SERVER_HOST = '127.0.0.1'  # Replace with the server's IP address or hostname
+SERVER_PORT = 12345
+
+# Create a socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+client_socket.connect((SERVER_HOST, SERVER_PORT))
+
+
+
+
+
+
+app_window = pyautogui.getWindowsWithTitle('Notepad')[0]
+
+left, top, right, bottom = app_window.left, app_window.top, app_window.right, app_window.bottom
+screenshot = ImageGrab.grab(bbox=(left, top, right, bottom))
+new_width = screenshot.width // 2
+new_height = screenshot.height // 2
+resized_screenshot = screenshot.resize((new_width, new_height))
+resized_screenshot.save('screenshot.png')
+
+with open("screenshot.png", "rb") as file:
+    data = file.read(1024)
+    while data:
+        client_socket.send(data)
+        data = file.read(1024)
+
+print("[+] Picture sent to the server.")
+
+# Close the socket
+client_socket.close()
