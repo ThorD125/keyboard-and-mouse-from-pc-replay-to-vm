1e25a0df092250aff76e7727d681ea2d30f6e6c8
Ther <thor.demeestere@telenet.be>
2023-10-12 01:12:25 +0200

    f

--- client/client mouse test.py
+++ client/client mouse test.py
@@ -17,12 +17,35 @@ import mouse
 # print(recorded)
 
 # recorded = []
-def test(x):
-    print(f"{x.x},{x.y}")
 
-mouse.hook(test)
+globals()['prev_mouse_pos'] = None
+
+# def namestr(obj):
+#     print(obj)
+#     print(locals())
+#     namespace = locals()
+#     print([name for name in namespace if namespace[name] is obj])
+
+host = '192.168.32.156'
+port = 12345
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((host, port))
+
+def send_mouse(x):
+    data = f"{x.x},{x.y}"
+    if data != globals()['prev_mouse_pos']:
+        globals()['prev_mouse_pos'] = data
+        
+        print(data)
+
+        message = struct.pack('!I', len(data)) + data.encode('utf-8')
+        client_socket.sendall(message)
+
+
+mouse.hook(send_mouse)
 mouse.wait(button=mouse.RIGHT, target_types=(mouse.DOWN,))
-mouse.unhook(test)
+mouse.unhook(send_mouse)
 
 
 # print(recorded)
